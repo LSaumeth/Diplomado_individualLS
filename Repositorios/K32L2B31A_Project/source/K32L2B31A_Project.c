1323ed3979512afd79d43be34273e3f4ad95ee94
leisman <xxleismanxx@gmail.com>
2021-09-10 18:40:40 -0500

    Proyecto sensor de Luz y Temperatura
    programacion y activacion de botones y sensores

--- Repositorios/K32L2B31A_Project/source/K32L2B31A_Project.c
+++ Repositorios/K32L2B31A_Project/source/K32L2B31A_Project.c
@@ -1,37 +1,16 @@
-/*
- * Copyright 2016-2021 NXP
- * All rights reserved.
+/*! @file : K32L2B31A_Project.c
+ * @author  Leisman Alberto Saumeth Valdeblanquez
+ * @version 0.0.000
+ * @date    23/08/2021
+ * @brief   Funcion principal main
+ * @details
+ *			v0.0.000	Proyecto base creado usando MCUXpresso
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
  *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-/**
- * @file    K32L2B31A_Project.c
- * @brief   Application entry point.
- */
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -39,14 +18,38 @@
 #include "clock_config.h"
 #include "K32L2B31A.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
 
-/* TODO: insert other definitions and declarations here. */
+#include "leds.h"
+#include "sensor_de_luz.h"
+#include "irq_lptmr0.h"
+#include "botones.h"
+#include "sensor_de_Temperatura.h"
 
-/*
- * @brief   Application entry point.
- */
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Private Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * External vars
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Local vars
+ ******************************************************************************/
+unsigned int test_global_var=100;
+float dato_float=3.1416;
+/*******************************************************************************
+ * Private Source Code
+ ******************************************************************************/
+
+
 int main(void) {
+	float sensor_de_luz, sensor_de_Temperatura;
+	bool boton1_activado,boton2_activado;
 
     /* Init board hardware. */
     BOARD_InitBootPins();
@@ -57,16 +60,59 @@ int main(void) {
     BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World\n");
+    PRINTF("Hello World\r\n");
+    printf("test_global_var:%d\r\n",test_global_var);
+    printf("dato_float:%g\r\n",dato_float);
+    /* Start counting */
+        LPTMR_StartTimer(LPTMR0);
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
+
+
+
+
     while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
+
+    	if(lptmr0_irq_counter != 0){
+
+
+    		lptmr0_irq_counter = 0;
+
+    		boton1_activado = !boton1LeerEstado();
+    		boton2_activado = !boton2LeerEstado();
+
+    		if(boton1_activado && !flag_boton1_presionado){
+    			flag_boton1_presionado = 1;
+    			sensor_de_luz = SensorDeLuzObtenerDatoADC();
+    			printf("luminosidad en lux: %.3f\r\n", sensor_de_luz);
+    		}
+
+    		if(!boton1_activado){
+    			flag_boton1_presionado = 0;
+    		}
+
+    		if(boton2_activado && !flag_boton2_presionado){
+    			flag_boton2_presionado = 1;
+    			sensor_de_Temperatura = SensorDeTemperaturaObtenerDatoADC();
+    			printf("Temperatura en grados: %.3f\r\n", sensor_de_Temperatura);
+
+
+    		}
+
+    		if(!boton2_activado){
+    			flag_boton2_presionado = 0;
+    		}
+
+    	}
+
+
+
+    		if(flag_led_rojo_iqr_counter == 1000){
+
+    		   flag_led_rojo_iqr_counter = 0;
+    		   toggle_led_rojo();
+    	}
     }
-    return 0 ;
+
+
+    return 0;
 }
---
+++ Repositorios/K32L2B31A_Project/source/gpio.h
@@ -0,0 +1,12 @@
+ /* gpio.h
+ *
+ *  Created on: 29/08/2021
+ *      Author: Leisman Alberto Saumeth Valdeblanquez
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+
+
+#endif /* GPIO_H_ */
